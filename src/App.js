9cf5e6f46c4c059467a1da2c08fab50504fe6e91
Waikikec <martin.d.yankov@gmail.com>
2021-11-08 11:52:39 +0200

    initialize react default  application

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import TopBar from "./navbar/NavBar";
+
+function App() {
+  return (
+    <>
+      <TopBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/navbar/NavBar.jsx
@@ -0,0 +1,11 @@
+import './navbar.css';
+
+export default function TopBar() {
+    return (
+        <div className="nav">
+            <div className="navLeft"></div>
+            <div className="navCenter"></div>
+            <div className="navRight"></div>
+        </div>
+    )
+}
