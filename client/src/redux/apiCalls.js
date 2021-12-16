d0554ca470d2e06845956e3276a4666bae7ed233
Waikikec <martin.d.yankov@gmail.com>
2021-12-16 09:18:31 +0200

    Minor Changes

--- client/src/redux/apiCalls.js
+++ client/src/redux/apiCalls.js
@@ -10,12 +10,12 @@ import {
     updateSuccess,
     updateFailure,
 } from './userRedux';
-import { userRequest } from '../utils/requestMethods';
+import { publicRequest, userRequest } from '../utils/requestMethods';
 
 export const login = async (dispatch, user) => {
     dispatch(loginStart());
     try {
-        const res = await userRequest.post('/auth/login', user);
+        const res = await publicRequest.post('/auth/login', user);
         dispatch(loginSuccess(res.data));
     } catch (error) {
         dispatch(loginFailure());
@@ -25,7 +25,7 @@ export const login = async (dispatch, user) => {
 export const register = async (dispatch, user) => {
     dispatch(registerStart());
     try {
-        const res = await userRequest.post('/auth/register', user);
+        const res = await publicRequest.post('/auth/register', user);
         dispatch(registerSuccess(res.data));
     } catch (error) {
         dispatch(registerFailure());
--- client/src/utils/requestMethods.js
+++ client/src/utils/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "http://localhost:5000/api";
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.currentUser;
 const currentUser = user && JSON.parse(user);
